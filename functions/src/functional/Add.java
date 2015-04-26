18dc98e572f322c57364131e60a26f825132404b
VallyRay <vallynray@gmail.com>
2015-04-26 18:24:37 +0400

    Cleaned Add.java

--- functions/src/functional/Add.java
+++ functions/src/functional/Add.java
@@ -4,96 +4,88 @@ import model.Country;
 import model.Holiday;
 import model.Tradition;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Add {
-    public static boolean uniqueFlag = true;
 
-
-    private Add(){
-
-    }
-    private static boolean isUnique(Object value, List<?> list){
-        boolean result = true;
-        for (Object item : list){
+    // Проверка на уникальность объекта
+    private static boolean isUnique(Object value, List<?> list) {
+        for (Object item : list) {
             if (value.toString().equals(item.toString())) {
-                result = false;
+                return false;
             }
         }
-        return result;
+        return true;
     }
-    //Добавить страну.
-    public static List<Country> addCountry(String name, List<Country> list){
+
+    //region Добавление страны
+    public static List<Country> addCountry(String name, List<Country> list) {
         Country country = new Country(name);
         if (isUnique(country, list)) {
             list.add(country);
         }
-        else uniqueFlag = false;
         return list;
     }
-    public static List<Country> addCountry(Country country, List<Country> list){
+
+    public static List<Country> addCountry(Country country, List<Country> list) {
         if (isUnique(country, list)) {
             list.add(country);
         }
-        else uniqueFlag = false;
         return list;
     }
-    //Добавить праздник.
-    public static List<Holiday> addHoliday(String name, List<Holiday> list){
+    //endregion
+
+    //region Добавление праздника
+    public static List<Holiday> addHoliday(String name, List<Holiday> list) {
         Holiday holiday = new Holiday(name);
         if (isUnique(holiday, list)) {
             list.add(holiday);
         }
-        else uniqueFlag = false;
         return list;
     }
-    public static List<Holiday> addHoliday(String name, int typeNum, List<Holiday> list){
+
+    public static List<Holiday> addHoliday(String name, int typeNum, List<Holiday> list) {
         Holiday holiday = new Holiday(name, typeNum);
         if (isUnique(holiday, list)) {
             list.add(holiday);
         }
-        else uniqueFlag = false;
         return list;
     }
-    public static List<Holiday> addHoliday(String name, Date start, int typeNum, List<Holiday> list){
+
+    public static List<Holiday> addHoliday(String name, Date start, int typeNum, List<Holiday> list) {
         Holiday holiday = new Holiday(name, start, typeNum);
         if (isUnique(holiday, list)) {
             list.add(holiday);
         }
-        else uniqueFlag = false;
         return list;
     }
+
     public static List<Holiday> addHoliday(String name, Date start, Date end, int typeNum,
-                                           List<Holiday> list){
+                                           List<Holiday> list) {
         Holiday holiday = new Holiday(name, start, end, typeNum);
         if (isUnique(holiday, list)) {
             list.add(holiday);
         }
-        else uniqueFlag = false;
         return list;
     }
-    //Добавить новую традицию.
+    //endregion
+
+    //region Добавление традиции
     public static List<Tradition> addTradition(Holiday holiday, Country country, List<Tradition> list) {
         Tradition tradition = new Tradition(holiday, country);
         if (isUnique(tradition, list)) {
-        list.add(tradition);
-        }
-        return list;
-    }
-    public static List<Tradition> addTradition(Holiday holiday, Country country, String description, List<Tradition> list) {
-        Tradition tradition = new Tradition(holiday, country);
-        tradition.setDescription(description);
-        if (isUnique(tradition, list)) {
-        list.add(tradition);
+            list.add(tradition);
         }
         return list;
     }
 
-    public static void checkTraditionList(Tradition tradition, ArrayList<Tradition> traditions){
-        if (!isUnique(tradition, traditions)){
-            traditions.remove(tradition);
+    public static List<Tradition> addTradition(Holiday holiday, Country country, String description, List<Tradition> list) {
+        Tradition tradition = new Tradition(holiday,country,description);
+        if (isUnique(tradition, list)) {
+            list.add(tradition);
         }
+        return list;
     }
+    //endregion
 }
